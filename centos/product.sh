a87b25e755e2d11dcdba64e6726378bb908abfba
zhu2688 <zhu2688@gmail.com>
2019-11-12 15:31:42 +0800

    update product.sh

--- centos/product.sh
+++ centos/product.sh
@@ -11,13 +11,14 @@ PHP_DISABLE_FUNCTIONS="show_source,system,shell_exec,passthru,exec,proc_open,pro
 PHP_YAF_ENVIRON=`/usr/bin/env php --ri yaf | grep yaf.environ | awk '{print $5}'`
 
 REDIS_CONF="/etc/redis/redis.conf"
+REDIS_BIND_ADDRESS="127.0.0.1"
 REDIS_DATA_PATH="/data/redis"
 REDIS_MAX_MEMORY="150Mb"
 REDIS_APPEND_ONLY="yes"
 REDIS_DAEMONIZE="no"
 
 MYSQL_CONF="/etc/my.cnf"
-MYSQL_ADDRESS="127.0.0.1"
+MYSQL_BIND_ADDRESS="127.0.0.1"
 
 ## 1 php config
 if ! command -v php
@@ -57,7 +58,7 @@ if [ ! -f "$REDIS_CONF" ]; then
 fi
 
 /bin/mkdir -p ${REDIS_DATA_PATH}/{logs,data}
-/bin/sed -i "s/^bind.*/bind 127.0.0.1/" "$REDIS_CONF"
+/bin/sed -i "s/^bind.*/bind ${REDIS_BIND_ADDRESS}/" "$REDIS_CONF"
 # sed -i 's/^port 6379/port '${REDIS_PORT}'/' "$REDIS_CONF"
 /bin/sed -i "s/# maxmemory <bytes>/maxmemory ${REDIS_MAX_MEMORY}/" "$REDIS_CONF"
 /bin/sed -i "s/# maxmemory-policy noeviction/maxmemory-policy volatile-lru/" "$REDIS_CONF"
